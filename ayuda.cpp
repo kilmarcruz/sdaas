4ad11b1f9663fca7205aadbaf2c630c93aee20fc
Fernanda Galdamez <128849114+feeeerzz@users.noreply.github.com>
2023-11-30 22:36:27 -0600

    feat: 😔

---
+++ ayuda.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int numero1, numero2, suma;
+
+int main() {
+
+  cout << "Ingrese el primer numero: ";
+    cin >> numero1;
+
+    cout << "Ingrese el segundo numero: ";
+    cin >> numero2;
+
+    suma = numero1 + numero2;
+    cout << "La suma de " << numero1 << " y " << numero2 << " es: " << suma << endl;
+
+    return 0;
+}
