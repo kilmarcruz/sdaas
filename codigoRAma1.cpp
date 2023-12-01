1cc41212c78c4600c324ddcbb92e5114820aa2c9
kilmarcruz <00043523@uca.edu.sv>
2023-11-30 22:36:32 -0600

    sda

---
+++ codigoRAma1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int decision;
+    cout<<"hola";
+    cout<<"hola rama1";
+    cout<<"cambios de rama1";
+    cin>>decision;
+    for (int i = 0; i < decision; i++)
+    {
+        cout<<"hola";
+    }
+    
+
+    return 0;
+}
